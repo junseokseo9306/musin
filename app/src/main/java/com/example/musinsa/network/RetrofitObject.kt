9f9fd836d3fd305a5430dfe9ec1bd5f90a0cbadf
junseokseo <junseoks9306@gmail.com>
2022-07-14 11:38:10 +0900

    feat: OkHttp 및 Retrofit 구현체 작성

---
+++ app/src/main/java/com/example/musinsa/network/RetrofitObject.kt
@@ -0,0 +1,41 @@
+package com.example.musinsa.network
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RetrofitObject {
+    private const val BASE_URL = "https://meta.musinsa.com/"
+
+    @Provides
+    @Singleton
+    fun provideOkHttp(): OkHttpClient {
+        val logger = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+        return OkHttpClient.Builder()
+            .addInterceptor(logger)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideItemListApi(
+        okHttpClient: OkHttpClient,
+    ): ApiClient {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ApiClient::class.java)
+    }
+}
